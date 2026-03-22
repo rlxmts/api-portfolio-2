33456e75e9e7fa48d40cb9fba994eaa2a7df51f8
Matheus <rlxmts@gmail.com>
2026-03-22 14:52:15 -0300

    chore: incluindo nova rota / post

--- src/controllers/projetosController.js
+++ src/controllers/projetosController.js
@@ -9,6 +9,15 @@ class projetoController {
             res.status(500).json({ message: `Erro ao buscar projetos: ${err}` });
         }
     }
+
+    static async cadastrarProjetos (req, res) {
+        try{
+            const novoProjeto = projeto.create(req.body);
+            res.status(200).json({ message: "Projeto incluido com sucesso!", projeto: novoProjeto });
+        }catch(err){
+            res.status(500).json({ message: `Erro ao cadastrar projetos: ${err}` });
+        }
+    }
 }
 
 export default projetoController;
--- src/routes/projetosRoutes.js
+++ src/routes/projetosRoutes.js
@@ -4,5 +4,6 @@ import projetoController from "../controllers/projetosController.js";
 const routes = express.Router();
 
 routes.get('/projetos', projetoController.buscaProjetos);
+routes.post('/projetos', projetoController.cadastrarProjetos);
 
 export default routes;
