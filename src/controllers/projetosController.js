f3c771105817d959a99909c55b83ad61ccaaee84
Matheus <rlxmts@gmail.com>
2026-03-23 11:18:45 -0300

    chore: criando ultimas rotas -

--- src/controllers/projetosController.js
+++ src/controllers/projetosController.js
@@ -12,10 +12,30 @@ class projetoController {
 
     static async cadastrarProjetos (req, res) {
         try{
-            const novoProjeto = projeto.create(req.body);
+            const novoProjeto = await projeto.create(req.body);
             res.status(200).json({ message: "Projeto incluido com sucesso!", projeto: novoProjeto });
         }catch(err){
             res.status(500).json({ message: `Erro ao cadastrar projetos: ${err}` });
+        }
+    }
+
+    static async editarProjeto (req, res) {
+        try{
+            const id = req.params.id;
+            await projeto.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Atalização concluida" });
+        }catch(err){
+            res.status(500).json({ message: `Erro ao editar projeto: ${err}` });
+        }
+    }
+
+    static async excluiProjeto (req, res){
+        try{
+            const id = req.params.id;
+            await projeto.findByIdAndDelete(id);
+            res.status(200).json({ message: "Projeto removido!" });
+        }catch(err){
+            res.status(500).json({ message: `Erro ao excluir: ${err} `});
         }
     }
 }
